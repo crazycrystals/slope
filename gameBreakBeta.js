83dbcb319c115de71ff79d3ffba2bcda881b23ff
Crazy Crystals <66223163+crazycrystals@users.noreply.github.com>
2023-04-17 15:00:24 -0500

    .
    heh

--- gameBreakBeta.js
+++ gameBreakBeta.js
@@ -1,22 +1,22 @@
 window.adsbygoogle = window.adsbygoogle || [];
 const adBreak = adConfig = function(o) {adsbygoogle.push(o);}
 adConfig({
-    preloadAdBreaks: 'on',
+    preloadAdBreaks: 'off',
     sound: 'on', // This game has sound
     onReady: () => {
-        console.log("ready");
+        console.log("Hopefully not ready :) - Adblock with to much effort");
     }, // Called when API has initialised and adBreak() is ready
 });
 function showNextAd()
 {
-    console.log("showNextAd")
+    console.log("showNextAd - hopefully wont work")
     adBreak({
         type: 'next', // ad shows at start of next level
         name: 'next-game',
         beforeAd: () => {            
             console.log("beforeAd")
             passBeforeAdData()
-        }, // You may also want to mute thegame's sound.
+        }, // You may also want to mute the game's sound.
         afterAd: () => {
             console.log("afterAd")
         }, // resume the game flow.
@@ -33,7 +33,7 @@ function showNextAd()
 
 function passBeforeAdData()
 {
-    myGameInstance.SendMessage('Canvas', 'pauseGame');
+    // myGameInstance.SendMessage('Canvas', 'pauseGame');
 }
 
 function adBreakDoneData()
